2ae4239d9885d0f687394112885d4a8b0439418a
Víctor López Ferrando <victorlopez90@gmail.com>
2016-01-25 12:46:31 +0100

    added README to code.html

--- print.sh
+++ print.sh
@@ -9,6 +9,8 @@ echo "<h2>templates/home.html</h2>" >> code.html
 pygmentize -f html -O full,linenos=1 templates/home.html >> code.html
 echo "<h2>templates/game.html</h2>" >> code.html
 pygmentize -f html -O full,linenos=1 templates/game.html >> code.html
-sed -i 's/pre { line-height: 125%; }/pre { line-height: 125%; font-size: 13px}/g' code.html
+#sed -i 's/pre { line-height: 125%; }/pre { line-height: 125%; font-size: 13px}/g' code.html
 echo "<h2>static/main.js</h2>" >> code.html
 pygmentize -f html -O full,linenos=1 static/main.js >> code.html
+echo "<h2>README.md</h2>" >> code.html
+pygmentize -f html -O full README.txt >> code.html
