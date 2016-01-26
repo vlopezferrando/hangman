2f235d82c95aba9761857bf1f92fecfad5af7970
Víctor López Ferrando <victorlopez90@gmail.com>
2016-01-26 11:32:41 +0100

    updated code pdf

--- docs/print.sh
+++ docs/print.sh
@@ -3,10 +3,17 @@
 CODE=docs/code.html
 
 cd ..
+echo '<!DOCTYPE html><html><head><meta charset="utf-8">' > $CODE
+echo '<style>h2{margin: 10px 0;}</style></head><body>' >> $CODE
 
-echo "<h2>Files</h2>" > $CODE
+echo "<h2>Files</h2>" >> $CODE
 echo "<pre>" >> $CODE
-tree -I docs | grep -v 'README\|pyc' >> $CODE
+paste \
+  <(tree -I docs | sed 's/^\./hangman/' | grep -v 'README\|pyc' | head -n -2 |
+    awk '{printf "%-28s\n", $0}') \
+  <(cloc . --exclude-dir=docs -quiet | tail -n +4 | head -n -3 |
+    sed -r 's/([a-zA-Z0-9]) {5}/\1/g' | sed -r 's/ {10}//' | sed -r 's/-{30}//') \
+  >> $CODE
 echo "</pre>" >> $CODE
 
 echo "<h2>hangman.py</h2>" >> $CODE
@@ -32,4 +39,5 @@ mv README.md README.txt
 pygmentize -f html -O full README.txt >> $CODE
 mv README.txt README.md
 
-sed -i 's/pre { line-height: 125%; }/pre { line-height: 125%; font-size: 14px}/g' $CODE
+echo "</body></html>" >> $CODE
+
